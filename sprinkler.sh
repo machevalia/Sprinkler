703b95ffaf54c8d66fea270e9d0001c4e6dc1ef3
Machevalia <36945847+machevalia@users.noreply.github.com>
2023-01-20 10:42:50 -0500

    Update sprinkler.sh
    Changing time control from hours & minutes to just hours in 2400 format to help with large lists which can lead to spraying outside authorized hours.

--- sprinkler.sh
+++ sprinkler.sh
@@ -11,14 +11,14 @@ RPCCLIENT=$(which rpcclient)
 NMBLOOKUP=$(which nmblookup)
 DOMAIN="$($NMBLOOKUP -A $HOST | grep GROUP | head -1 | awk '{print $1}')"
 # Set the current time
-CURRENT_TIME=$(date +%H%M)
+CURRENT_TIME=$(date +%H)
 
 # Loop until the current time is between argument #1 and #2
 while [[ 10#$CURRENT_TIME -lt 10#$START_TIME || 10#$CURRENT_TIME -ge 10#$STOP_TIME ]]; do
   # If the current time is outside of the start and stop times, sleep for 5 minutes
   sleep 300
   # Update the current time
-  CURRENT_TIME=$(date +%H%M)
+  CURRENT_TIME=$(date +%H)
 done
 
 # The script will reach this point when the current time is between the authorized hours
@@ -35,7 +35,7 @@ while read PASS; do
             echo "${DOMAIN}\\${USER}:${PASS} - LOGON SUCCESS at $(date)"
         fi
     done < <(cat "$USERS")
-    CURRENT_TIME=$(date +%H%M)
+    CURRENT_TIME=$(date +%H)
     if [[ $ITR -lt $LINES ]]; then
         echo -e "\e[33m[+]\e[0m Sleeping ${SLEEP} seconds."
         sleep ${SLEEP}
@@ -46,13 +46,13 @@ while read PASS; do
         echo "Pausing - outside authorized hours. Will resume spraying again once the system time is back within the authorized hours."
           
         # Update the current time       
-        CURRENT_TIME=$(date +%H%M)
+        CURRENT_TIME=$(date +%H)
         # Loop until the current time is the target time
         while [[ $(date +%s) -lt $TARGET_DATE ]]; do
             # Sleep for 1 minute
             sleep 60
             # Update the current time
-            CURRENT_TIME=$(date +%H%M)
+            CURRENT_TIME=$(date +%H)
         done
         # Jump back to the beginning of the spray loop
         continue
